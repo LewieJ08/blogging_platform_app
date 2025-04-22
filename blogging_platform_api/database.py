66f33cb25d9d1818e03e17f7df12df7d65325d58
LewieJ08 <lewiej08@gmail.com>
2025-04-22 01:32:08 +0100

    env variables setup

--- blogging_platform_api/database.py
+++ blogging_platform_api/database.py
@@ -1,3 +1,14 @@
+from dotenv import load_dotenv()
 import psycopg2
+import os 
 
-conn = psycopg2.connect(host="localhost", dbname="postgre")
+load_dotenv()
+
+host = os.getenv("HOST")
+dbname = os.getenv("DBNAME")
+user = os.getenv("USER")
+password = os.getenv("PASSWORD")
+port = os.getenv("PORT")
+
+conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password, port=port)
+
