c7ba1c4d056d86476205c76da7b072c500c031f8
LewieJ08 <lewiej08@gmail.com>
2025-04-25 10:34:21 +0100

    routing setup

--- blogging_platform_app/app.py
+++ blogging_platform_app/app.py
@@ -7,5 +7,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/create", methods=["GET","POST"])
+
+def create():
+    return render_template("create.html")
+
+@app.route("/update", methods=["GET","POST"])
+
+def update():
+    return render_template("update.html")
+
+@app.route("/delete", methods=["GET","POST"])
+
+def delete():
+    return render_template("delete.html")
+
+@app.route("/search", methods=["GET","POST"])
+
+def search():
+    return render_template("search.html")
+
 if __name__ == "__main__":
     app.run(debug=True)
